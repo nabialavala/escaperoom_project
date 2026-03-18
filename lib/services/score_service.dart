1e70394723bb30656ee520c6cd8b0516ce4a2cd2
Nabia Lavala <nabialavala@gmail.com>
2026-03-17 23:15:33 -0400

    Added score calculation service

---
+++ lib/services/score_service.dart
@@ -0,0 +1,30 @@
+class ScoreService {
+  int calculateScore({
+    required int timeSpent,
+    required int hintsUsed,
+    required int wrongAttempts,
+    required int difficultyBonus,
+  }) {
+    int score = 1000
+        - (timeSpent * 2)
+        - (hintsUsed * 50)
+        - (wrongAttempts * 25)
+        + (difficultyBonus * 100);
+
+    if (score < 0) {
+      return 0;
+    }
+
+    return score;
+  }
+
+  int getDifficultyBonus(int totalLevelsCompleted) {
+    if (totalLevelsCompleted >= 10) {
+      return 3;
+    } else if (totalLevelsCompleted >= 5) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+}
